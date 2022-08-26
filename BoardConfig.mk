43a3ce4380c58507a5b0d392e56d0da2089a05dd
noobyysauraj <gitsauraj@gmail.com>
2022-08-26 14:45:26 +0000

    sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609743540423 release-keys
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+DEVICE_PATH := device/realme/RMX2185
+BOARD_VENDOR := realme
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-08-05
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/realme/RMX2185/BoardConfigVendor.mk
